8eaa6890abbd2ad30f9bd0da874dcc216d07c573
Ocupuc <108402919+Ocupuc@users.noreply.github.com>
2023-11-06 13:28:03 +0500

    добавлена валидация для круга

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<String> _figureTypes = [];
   String? _selectedFigure;
+  final _formKey = GlobalKey<FormState>(); // Ключ для Form
   final _radiusController = TextEditingController();
   Map<String, String> _calculationResult = {};
 
@@ -27,12 +28,12 @@ class _MyAppState extends State<MyApp> {
   Future<void> _getFigureTypes() async {
     final response = await http.get(
       Uri.parse('http://192.168.31.5:8080/api/figure-types'),
-      headers: {'Content-Type': 'application/json; charset=UTF-8'}, // Указание кодировки
+      headers: {'Content-Type': 'application/json; charset=UTF-8'},
     );
 
     if (response.statusCode == 200) {
       setState(() {
-        _figureTypes = List<String>.from(json.decode(utf8.decode(response.bodyBytes))); // Декодирование тела ответа с правильной кодировкой
+        _figureTypes = List<String>.from(json.decode(utf8.decode(response.bodyBytes)));
       });
     } else {
       throw Exception('Failed to load figure types');
@@ -40,18 +41,21 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<void> _calculateCircle() async {
-    final response = await http.post(
-      Uri.parse('http://192.168.31.5:8080/api/figures/circle'),
-      headers: {'Content-Type': 'application/json; charset=UTF-8'}, // Указание кодировки
-      body: json.encode({'radius': double.tryParse(_radiusController.text)}),
-    );
+    if (_formKey.currentState?.validate() ?? false) {
+      // Если форма валидна, отправляем запрос
+      final response = await http.post(
+        Uri.parse('http://192.168.31.5:8080/api/figures/circle'),
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        body: json.encode({'radius': double.tryParse(_radiusController.text)}),
+      );
 
-    if (response.statusCode == 200) {
-      setState(() {
-        _calculationResult = Map<String, String>.from(json.decode(utf8.decode(response.bodyBytes))); // Декодирование тела ответа с правильной кодировкой
-      });
-    } else {
-      throw Exception('Failed to calculate circle parameters');
+      if (response.statusCode == 200) {
+        setState(() {
+          _calculationResult = Map<String, String>.from(json.decode(utf8.decode(response.bodyBytes)));
+        });
+      } else {
+        throw Exception('Failed to calculate circle parameters');
+      }
     }
   }
 
@@ -65,55 +69,74 @@ class _MyAppState extends State<MyApp> {
       ],
       supportedLocales: const [
         Locale('ru', 'RU'), // Русский язык
-        // Другие поддерживаемые языки
       ],
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Calculator of Geometric Figures'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              DropdownButton<String>(
-                value: _selectedFigure,
-                hint: const Text('Выберите тип фигуры'),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedFigure = newValue;
-                  });
-                },
-                items: _figureTypes.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              if (_selectedFigure == 'Круг') ...[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: _radiusController,
-                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Введите радиус круга',
+          child: Form(
+            key: _formKey, // Присваиваем ключ Form
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                DropdownButton<String>(
+                  value: _selectedFigure,
+                  hint: const Text('Выберите тип фигуры'),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedFigure = newValue;
+                    });
+                  },
+                  items: _figureTypes.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                if (_selectedFigure == 'Круг') ...[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _radiusController,
+                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Введите радиус круга',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Пожалуйста, введите радиус';
+                        }
+                        final number = double.tryParse(value);
+                        if (number == null) {
+                          return 'Введите корректное числовое значение';
+                        }
+                        if (number <= 0.01) { // Минимальное значение
+                          return 'Радиус должен быть больше 0.01';
+                        }
+                        if (number > 10000) { // Максимальное значение
+                          return 'Радиус должен быть меньше или равен 10000';
+                        }
+                        return null;
+                      },
+
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: _calculateCircle,
-                  child: const Text('Рассчитать'),
-                ),
-                if (_calculationResult.isNotEmpty) ...[
-                  Text('Периметр: ${_calculationResult['perimeter']}'),
-                  Text('Площадь: ${_calculationResult['area']}'),
-                  Text(_calculationResult['description'] ?? ''),
-                ]
+                  ElevatedButton(
+                    onPressed: _calculateCircle,
+                    child: const Text('Рассчитать'),
+                  ),
+                  if (_calculationResult.isNotEmpty) ...[
+                    Text('Периметр: ${_calculationResult['perimeter']}'),
+                    Text('Площадь: ${_calculationResult['area']}'),
+                    Text(_calculationResult['description'] ?? ''),
+                  ]
+                ],
+                // Добавьте виджеты для других фигур здесь
               ],
-              //
-            ],
+            ),
           ),
         ),
       ),
